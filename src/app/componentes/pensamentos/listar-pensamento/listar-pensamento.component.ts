17873859a8cbdd051508871fe26b600af194b490
Lucas Oliveira <lucas.oliveira@ituran.com.br>
2024-02-23 14:35:04 -0300

    Adicionado diretivas ngIf e ngClass

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -17,6 +17,11 @@ export class ListarPensamentoComponent implements OnInit {
       conteudo: 'Minha propriedade é decorada com @Input()',
       autoria: 'Componente filho',
       modelo: 'modelo2'
+    },
+    {
+      conteudo: 'Teste de tamanho - Teste de tamanho - Teste de tamanho - Teste de tamanho - Teste de tamanho - Teste de tamanho - Teste de tamanho - Teste de tamanho - Teste de tamanho - Teste de tamanho - Teste de tamanho - Teste de tamanho - Teste de tamanho - Teste de tamanho - Teste de tamanho - Teste de tamanho - Teste de tamanho - Teste de tamanho - Teste de tamanho - Teste de tamanho - Teste de tamanho - Teste de tamanho - Teste de tamanho - Teste de tamanho - Teste de tamanho - Teste de tamanho - Teste de tamanho - Teste de tamanho - Teste de tamanho - Teste de tamanho',
+      autoria: '',
+      modelo: 'modelo1'
     }
   ];
 
